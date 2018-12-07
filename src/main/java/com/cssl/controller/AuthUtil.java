c42630ae4f2a60925bf112983271c19d7034ae87
WuJiaXingGit <287475833@qq.com>
2018-12-07 15:08:33 +0800

    整合idea

--- src/main/java/com/cssl/controller/AuthUtil.java
+++ src/main/java/com/cssl/controller/AuthUtil.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 @Component
 public class AuthUtil {
     /**
-     *添加了注释ideaeffffffffffwddddddddddd
+     *添加了注释ideaeffffffffffwdddddddddddddddddddddddddsss
      */
 
     public static final String APPID="wxdf60633cea3de308";
---
+++ src/main/java/com/cssl/controller/LoginServlet.java
@@ -0,0 +1,26 @@
+package com.cssl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URLEncoder;
+
+@Controller
+public class LoginServlet {
+    /**
+     *
+     * @return
+     */
+    @RequestMapping("/wxLogin")
+    public String doget(){
+        String backUrl="http://134.175.102.188:8080/weixin/callBack";
+        String url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=" +AuthUtil.APPID
+                +"&redirect_uri=REDIRECT_URI" + URLEncoder.encode(backUrl)
+                +"&response_type=code" +
+                "&scope=snsapi_userinfo" +
+                "&state=STATE#wechat_redirect";
+        System.out.println("进来了wxlogin=================================================>");
+        return "redirect:"+url;
+    }
+
+}
